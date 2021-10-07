deb75be7b957e625ad266a288fdd5d27611f45eb
nguyenduyluc <duyluc9595@gmail.com>
2021-10-07 10:37:14 +0700

    Assignment 1 SQL

--- SQL/Assignment 1/bai tap 1.sql
+++ SQL/Assignment 1/bai tap 1.sql
@@ -1,22 +1,22 @@
-);DROP DATABASE IF EXISTS Testing_system_assignment_1;
+DROP DATABASE IF EXISTS Testing_system_assignment_1;
 CREATE DATABASE Testing_system_assignment_1;
 USE 			Testing_system_assignment_1;
 
 DROP TABLE IF EXISTS Department;
 CREATE TABLE 	Department(
-	DepartmentID 		INT,
+	DepartmentID 		INT AUTO_INCREMENT PRIMARY KEY,
 	DepartmentName   	VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS Position;
 CREATE TABLE  `Position`(
-	PositionID 			INT,
+	PositionID 			INT AUTO_INCREMENT PRIMARY KEY,
 	PositionName 		VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS `Account`;
 CREATE TABLE `Account`(
-	AccountID		INT,
+	AccountID		INT AUTO_INCREMENT PRIMARY KEY,
     Email 			VARCHAR(50),
     Username		VARCHAR(50),
     FullName		VARCHAR(50),
@@ -27,7 +27,7 @@ CREATE TABLE `Account`(
 
 DROP TABLE IF EXISTS `group`;
 CREATE TABLE `group`(
-	GroupID			INT,
+	GroupID			INT AUTO_INCREMENT PRIMARY KEY,
     GroupName		VARCHAR(50),
     CreatorID		INT,
     CreateDate		VARCHAR(50)
@@ -41,19 +41,19 @@ CREATE TABLE GroupAccount(
 );
 DROP TABLE IF EXISTS TypeQuestion;
 CREATE TABLE TypeQuestion(
-	TypeID			INT,
+	TypeID			INT AUTO_INCREMENT PRIMARY KEY,
     TypeName		VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS CategoryQuestion;
 CREATE TABLE CategoryQuestion(
-	CategoryID 			INT,
+	CategoryID 			INT AUTO_INCREMENT PRIMARY KEY,
 	CategoryName		VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS Question;
 CREATE TABLE Question(
-	QuestionID		INT,
+	QuestionID		INT AUTO_INCREMENT PRIMARY KEY,
 	content			VARCHAR(100),
     CategoryID		INT,
     TypeID			INT,
@@ -63,7 +63,7 @@ CREATE TABLE Question(
 
 DROP TABLE IF EXISTS Answer;
 CREATE TABLE Answer(
-	AnswerID		INT,
+	AnswerID		INT AUTO_INCREMENT PRIMARY KEY,
     Content			VARCHAR(100),
     QuestionID		INT,
     isCorrect		VARCHAR(100)
@@ -71,7 +71,25 @@ CREATE TABLE Answer(
 
 DROP TABLE IF EXISTS Exam;
 CREATE TABLE Exam(
+	ExamID			INT AUTO_INCREMENT PRIMARY KEY,
+    `code`			INT,
+    Title			VARCHAR(100),
+    CategoryID		INT,
+    Duration		VARCHAR(100),
+    CreatorID		INT,
+    CreateDate		VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS ExamQuestion;
+CREATE TABLE ExamQuestion(
 	ExamID			INT,
+    QuestionID		INT
+);
+
+
+DROP TABLE IF EXISTS Exam;
+CREATE TABLE Exam(
+	ExamID			INT AUTO_INCREMENT PRIMARY KEY,
     `code`			INT,
     Title			VARCHAR(100),
     CategoryID		INT,
